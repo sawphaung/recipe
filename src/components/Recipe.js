3b5effa70d48ff2fbf140a3bcb187f8f3696e94b
Saw Phaung <saw@77creative.io>
2019-09-08 09:58:33 -0500

    changes style

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -15,31 +15,33 @@ const Recipe = ({
       <div className='image-div'>
         <img src={image} alt='' />
       </div>
-      <h1 className='recipe-title'>{title}</h1>
-      <p className='calories'>
-        Calories: <span>{parseInt(calories)} Kcal</span>
-      </p>
-      <p className='calories'>
-        Time: <span>{time}</span> Minutes
-      </p>
-      <p className='calories'>
-        Total Weight: <span>{parseInt(totalWeight)} g</span>
-      </p>
+      <div className='recipe_contents'>
+        <h1 className='recipe-title'>{title}</h1>
+        <p className='calories'>
+          Calories: <span>{parseInt(calories)} Kcal</span>
+        </p>
+        <p className='calories'>
+          Time: <span>{time}</span> Minutes
+        </p>
+        <p className='calories'>
+          Total Weight: <span>{parseInt(totalWeight)} g</span>
+        </p>
 
-      <p className='calories'>
-        Serving: <span>{serving}</span>{' '}
-      </p>
+        <p className='calories'>
+          Serving: <span>{serving}</span>{' '}
+        </p>
 
-      <p>Ingredients:</p>
-      <ul>
-        {ingredients.map((ingredient, index) => (
-          <li key={index}>{ingredient.text}</li>
-        ))}
-      </ul>
+        <p>Ingredients:</p>
+        <ul>
+          {ingredients.map((ingredient, index) => (
+            <li key={index}>{ingredient.text}</li>
+          ))}
+        </ul>
 
-      <p className='calories'>
-        Source: <span>{source}</span>
-      </p>
+        <p className='calories'>
+          Source: <span>{source}</span>
+        </p>
+      </div>
     </div>
   );
 };
